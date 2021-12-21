8f702233d775c1073928f36a394abd879497ecca
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2021-12-22 06:33:22 +0700

    08-FerdyFebriyanto

--- cypress/integration/4-FerdyFebriyanto_EditProfile/testSkenario.spec.js
+++ cypress/integration/4-FerdyFebriyanto_EditProfile/testSkenario.spec.js
@@ -1,5 +1,12 @@
 describe('Test Skenario 1', () => {
     it('Success Edit', () => {
-        cy.visit('http://lms.polinema.ac.id/')
+        cy.visit('http://lms.polinema.ac.id/login/index.php')
+        cy.get('#username')
+            .type('1941720007')
+            .should('have.value', '1941720007')
+        cy.get('#password')
+            .type('1941720007')
+            .should('have.value', '1941720007')
+        cy.get('#loginbtn').click()
     })
 })
