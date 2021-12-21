b60b08f3e8487584026c0d1a964bb1a91c6f637d
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2021-12-21 18:33:00 +0700

    08-FerdyFebriyanto

---
+++ cypress/integration/4-FerdyFebriyanto_EditProfile/testSkenario1.spec.js
@@ -0,0 +1,5 @@
+describe('Test Skenario 1', () => {
+    it('Success Edit', () => {
+        cy.visit('http://lms.polinema.ac.id/')
+    })
+})
--- cypress/integration/4-FerdyFebriyanto_Login/test.spec.js
+++
@@ -1,5 +0,0 @@
-describe('Edit Profile', () => {
-    it('Does not do much!', () => {
-        expect(true).to.equal(false)
-    })
-})
