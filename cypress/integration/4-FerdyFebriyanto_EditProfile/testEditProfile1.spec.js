e332840159de06e82540c9b24ddbfc354321361a
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2021-12-22 10:43:54 +0700

    08-FerdyFebriyanto

--- cypress/integration/4-FerdyFebriyanto_EditProfile/testEditProfile1.spec.js
+++ cypress/integration/4-FerdyFebriyanto_EditProfile/testEditProfile1.spec.js
@@ -1,6 +1,9 @@
 describe('Edit Profile Scenario - 1941720007 / Ferdy Febriyanto', () => {
-    it('Edit Profile Berhasil', () => {
+    beforeEach(() => {
         cy.visit('http://lms.polinema.ac.id/login/index.php')
+    })
+    it('Edit Profile Berhasil', () => {
+
         cy.get('#username')
             .type('1941720007')
             .should('have.value', '1941720007')
@@ -10,7 +13,8 @@ describe('Edit Profile Scenario - 1941720007 / Ferdy Febriyanto', () => {
         cy.get('#loginbtn').click()
 
         cy.url().should('include', '/my/')
+        cy.contains('Profile').children('i').should('have.class', 'fa-user').click({ force: true })
 
-        cy.get('.usermenu').contains('#actionmenuaction-2').click()
+        cy.visit('/user/edit.php?id=14887&course=1')
     })
 })
