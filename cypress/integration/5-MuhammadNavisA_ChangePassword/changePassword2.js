2a888b3fad7ff140fafa6a16021e18c3a5c3fc46
= <=>
2021-12-22 13:01:56 +0700

    14-MuhammadNavisA

--- cypress/integration/5-MuhammadNavisA_ChangePassword/changePassword2.js
+++ cypress/integration/5-MuhammadNavisA_ChangePassword/changePassword2.js
@@ -4,24 +4,23 @@ describe('Login', function (){
         // Halaman Login
         cy.get('#username').type('1941720147')
         // Input Username
-        cy.get('#password').type('1941720147')
+        cy.get('#password').type('malangan')
         // Input Password                
         cy.get('[class="btn btn-primary btn-block m-t-1"]').should('be.visible').click()   
         // Button Login
     })
-    it('Change Password', () => {  
+    it('Change Password Dengan Angka', () => {  
         cy.visit('http://lms.polinema.ac.id/user/preferences.php')
         // Masuk Halaman Pengaturan
         cy.visit('http://lms.polinema.ac.id/login/change_password.php?id=1')
         // Masuk Halaman Change Password
-        cy.get('#id_password').type('1941720147')
+        cy.get('#id_password').type('malangan')
         // Input Current Password
-        cy.get('#id_newpassword1').type('1malangan12')
+        cy.get('#id_newpassword1').type('1941720147')
         // Input New Password 1
-        cy.get('#id_newpassword2').type('malangan12')
+        cy.get('#id_newpassword2').type('1941720147')
         // Input New Password 2
         cy.get('#id_submitbutton').should('be.visible').click()
         // Button Submit 
     })
-})    
-    
+})
